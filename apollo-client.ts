c798acd0e4b53947c62ea1a57036212e416e7093
callisto28 <iotaseb@gmail.com>
2022-06-30 13:20:08 +0200

    test https client

--- apollo-client.ts
+++ apollo-client.ts
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const GRAPHQL_URL = "http://localhost:3001/graphql";
 // const HEROKU_URL = process.env.NEXT_PUBLIC_HEROKU_URL;
-const HEROKU_URL = "http://nestvinyl.herokuapp.com/graphql";
+const HEROKU_URL = "https://nestvinyl.herokuapp.com/graphql";
 
 const client = new ApolloClient({
   uri: HEROKU_URL,
