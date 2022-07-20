693f0976ecbc10c3565c3092de62437955808b66
seb-azot <sebastien.fourny@azot.dev>
2022-07-20 23:45:15 +0200

    TEST[fallback:true id article]

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   target: 'serverless',
   images: {
-    domains: ['media.graphcms.com', 'static.fnac-static.com', 'www.musiconvinyl.com', 'i.ytimg.com', 'm.media-amazon.com', 'get.pxhere.com', 'cdn.pixabay.com', 'us.123rf.com', 'dfxqtqxztmxwe.cloudfront.net', "www.mesdisquesvinyles.com", "www.abondance.com"],
+    domains: ['media.graphcms.com', 'static.sonovente.com', 'static.fnac-static.com', 'www.musiconvinyl.com', 'i.ytimg.com', 'm.media-amazon.com', 'get.pxhere.com', 'cdn.pixabay.com', 'us.123rf.com', 'dfxqtqxztmxwe.cloudfront.net', "www.mesdisquesvinyles.com", "www.abondance.com"],
   },
 }
 
