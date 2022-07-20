f8d0d4c5685b61ac1519a854f72ed63d59230fc1
seb-azot <sebastien.fourny@azot.dev>
2022-07-20 22:33:43 +0200

    TEST[fetchpolicy network-only]

--- apollo-client.ts
+++ apollo-client.ts
@@ -4,13 +4,22 @@ const GRAPHQL_URL = "http://localhost:3001/graphql";
 // const HEROKU_URL = process.env.NEXT_PUBLIC_HEROKU_URL;
 const HEROKU_URL = "https://nestvinyl.herokuapp.com/graphql";
 
+
+
 const client = new ApolloClient({
   uri: HEROKU_URL,
   cache: new InMemoryCache(),
   defaultOptions: {
     watchQuery: {
+      fetchPolicy: 'cache-and-network',
+      nextFetchPolicy: 'network-only',
+    },
+    query: {
       fetchPolicy: 'network-only',
-        // nextFetchPolicy: 'network-only',
+      errorPolicy: 'all',
+    },
+    mutate: {
+      errorPolicy: 'all',
     },
   },
 });
