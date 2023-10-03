4de6e3eafff00d975c68e7cadd9b561c18197dd8
seb-azot <sebastien.fourny@azot.dev>
2023-10-03 22:11:51 +0200

    MAJ

--- apollo-client.ts
+++ apollo-client.ts
@@ -3,6 +3,7 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 const GRAPHQL_URL = "http://localhost:3001/graphql";
 // const HEROKU_URL = process.env.NEXT_PUBLIC_HEROKU_URL;
 const HEROKU_URL = "https://nestvinyl.herokuapp.com/graphql";
+//const O2SWITCH_URL = "https://o2switch.com/graphql";
 
 const client = new ApolloClient({
   uri: HEROKU_URL,
